c475d8fe61047b850ec001f09b4d2f139f789e72
Ishwar <ishwar@riverheadsoft.com>
2026-04-25 21:10:47 +0530

    Object in javascript (core js)

---
+++ topics/core-javaScript/data-types/objects/object.js
@@ -0,0 +1,11 @@
+const user = {
+  name: "Ishwar",
+  age: 25,
+  isLoggedIn: true,
+};
+
+// console.log("======= name ", user.name); // Ishwar
+// console.log("======= age ", user.age); // 25
+// console.log("======= name ", user.isLoggedIn); // true
+
+console.log("=======", this.name);
---
+++ topics/core-javaScript/data-types/objects/readme.md
@@ -0,0 +1,7 @@
+# JavaScript Object
+
+Objects are non-primitive data types used to store collections of data.
+
+- Stored by reference
+- Mutable
+- Can contain multiple values (key-value pairs)
