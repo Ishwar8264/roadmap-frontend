d3a4052b268e91615c53986dee6a5b807e57e551
Ishwar <ishwar@riverheadsoft.com>
2026-04-27 18:25:39 +0530

    Typescript(static type): like interfaces,enum and type aliases

---
+++ topics/typeScript/Static/index.ts
@@ -0,0 +1,19 @@
+//! interface;
+
+//* use case
+
+// Mostly use ):-  objects / API response / props
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+
+const user: User = {
+  id: "1",
+  name: "Ishwar",
+  email: "ishwar@gmail.com",
+  isActive: true,
+};
---
+++ topics/typeScript/enum/index.ts
@@ -0,0 +1,11 @@
+//* enum (Fixed set of values)
+
+enum Status_1 {
+  PENDING = "PENDING",
+  SUCCESS = "SUCCESS",
+  ERROR = "ERROR",
+}
+
+const status_1: Status_1 = Status_1.SUCCESS;
+
+console.log(status_1);
